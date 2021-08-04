db00ba16128ce30903d55968ef6be97f6f2f2928
Namacha411 <52745962+Namacha411@users.noreply.github.com>
2021-08-04 23:21:50 +0900

    formのドラッグの実装

--- desktop-pet/Form1.cs
+++ desktop-pet/Form1.cs
@@ -12,9 +12,57 @@ namespace desktop_pet
 {
 	public partial class Form1 : Form
 	{
+		private bool isDragging;
+		private Point mousePointerOffset;
+
 		public Form1()
 		{
+			// Form1 init
 			InitializeComponent();
+			FormBorderStyle = FormBorderStyle.None;
+			TopMost = true;
+			ClientSize = new Size(255, 255);
+
+			// event handler init
+			MouseDown += new MouseEventHandler(Form1_LeftClickDown);
+			MouseUp += new MouseEventHandler(Form1_LeftClickUp);
+			MouseMove += new MouseEventHandler(Form1_MouseMove);
+
+			// variable init
+			isDragging = false;
 		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void Form1_LeftClickDown(object sender, MouseEventArgs e)
+		{
+			if (e.Button == MouseButtons.Left)
+			{
+				this.isDragging = true;
+				this.mousePointerOffset = new Point(e.X, e.Y);
+			}
+		}
+
+		private void Form1_MouseMove(object sender, MouseEventArgs e)
+		{
+			if (this.isDragging)
+			{
+				var nextPoint = PointToScreen(new Point(e.X, e.Y));
+				nextPoint.Offset(-1 * mousePointerOffset.X, -1 * (mousePointerOffset.Y + SystemInformation.CaptionHeight) + 24);
+				Location = nextPoint;
+			}
+		}
+
+		private void Form1_LeftClickUp(object sender, MouseEventArgs e)
+		{
+			if (e.Button == MouseButtons.Left)
+			{
+				this.isDragging = false;
+			}
+		}
+
 	}
 }
